7f81726e3e64bd2444a582981cc380c55dac9f72
Admin <Admin@DESKTOP-9VS7AV9>
2024-04-01 11:54:00 +0800

    First Commit Baysaa

---
+++ baysaa_gang/src/baysaa_gang/baysaa247.java
@@ -0,0 +1,15 @@
+package baysaa_gang;
+
+import java.util.Scanner;
+
+public class baysaa247 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		int n =220;
+		int k = 27;
+		System.out.println("Baysaa"+(n+k));
+	}
+
+}
